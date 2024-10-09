b0d68f0c83b95741f3e208b842b73e6ee25a6844
Anton J Olsson <anton.j.olsson@bredband.net>
2024-10-10 00:00:00 +0200

    feat: 🎸 add more statistics!

--- linear-regression.js
+++ linear-regression.js
@@ -1,5 +1,8 @@
-const xsString = "1.0, 1.0, 1.0, 1.5, 1.5, 1.5, 1.75, 1.75, 1.75, 2.5, 2.5, 2.5, 3.0, 3.0, 3.0, 4.0, 4.0, 4.0, 6.0, 6.0, 6.0, 10.0, 10.0, 10.0"
-const ysString = "525, 520, 527, 785, 780, 790, 915, 900, 922, 1300, 1295, 1310, 1575, 1565, 1582, 2100, 2110, 2090, 3125, 3120, 3133, 5250, 5256, 5245"
+const xsString = "20.0 30.5 40.0 55.1 60.3 74.9 88.4 95.2"
+const ysString = "1.8 3.0 4.8 5.0 6.5 7.0 9.0 9.1"
+
+const b0NullHypothesisValue = 0
+const b1NullHypothesisValue = 0
 
 const xs = xsString.split(/,?\s/)
     .map(x => Number(x))
@@ -11,21 +14,52 @@ const n = xs.length
 const xSum = xs.reduce((p, c) => p + c, 0)
 const ySum = ys.reduce((p, c) => p + c, 0)
 
-let xSquaresSum = /*0*/ xs.reduce((p, c) => p + c ** 2, 0)
-let ySquaresSum = /*0*/ ys.reduce((p, c) => p + c ** 2, 0)
+const meanX = (xSum / n)
+const meanY = (ySum / n)
+
+const xSquaresSum = /*0*/ xs.reduce((p, c) => p + c ** 2, 0)
+const ySquaresSum = /*0*/ ys.reduce((p, c) => p + c ** 2, 0)
 
 const xySum = xs.reduce((p, c, i) => p + c * ys[i], 0)
 
+const sxx = xs.reduce((p, c) => p + (c - meanX) ** 2, 0)
+const syy = ys.reduce((p, c) => p + (c - meanY) ** 2, 0)
+const sxy = xs.reduce((p, c, i) => p + (c - meanX) * (ys[i] - meanY), 0)
+
 const b1 = (n * xySum - xSum * ySum) / (n * xSquaresSum - Math.pow(xSum, 2))
-const b0 = (ySum / n) - b1 * (xSum / n)
+const b0 = meanY - b1 * meanX
+
+const sse = xs.reduce((p, c, i) => p + (ys[i] - b0 - b1 * c) ** 2, 0)
+const variance = sse / (n - 2)
+const sd = Math.sqrt(variance)
+
+const b0H0TestStatistic = (b0 - b0NullHypothesisValue) / ((sd * Math.sqrt(xSquaresSum)) / Math.sqrt(n * sxx))
+const b1H0TestStatistic = b1 / (sd / Math.sqrt(sxx))
 
 console.log(`x values: ${xs}`)
 console.log(`y values: ${ys}`)
-console.log(`Data zipped: ${xs.map((x, i) => `{${x},${ys[i]}}`)}`)
+console.log(`Data paired: ${xs.map((x, i) => `{${x},${ys[i]}}`)}`)
 console.log(`n: ${n}`)
-console.log(`Sum of xs: ${xSum}`)
+console.log(`Mean of x: ${meanX}`)
+console.log(`Mean of y: ${meanY}`)
+
+console.log(`\nSum of xs: ${xSum}`)
 console.log(`Sum of ys: ${ySum}`)
 console.log(`Sum of xs squared: ${xSquaresSum}`)
 console.log(`Sum of ys squared: ${ySquaresSum}`)
 console.log(`Sum of xs times ys: ${xySum}`)
-console.log(`ŷ = ${b0} + ${b1}x`)
+
+console.log(`\nSxx: ${sxx}`)
+console.log(`Syy: ${syy}`)
+console.log(`Sxy: ${sxy}`)
+console.log(`SSE: ${sse}`)
+
+console.log(`\nŷ = ${b0} + ${b1}x`)
+console.log(`Variance: ${variance}`)
+console.log(`SD: ${sd}`)
+
+console.log(`\nTest statistic (T${n - 2}) for b0 null hypothesis (${b0NullHypothesisValue}): ${b0H0TestStatistic}`)
+console.log(`Test statistic (T${n - 2}) for b1 null hypothesis (${b1NullHypothesisValue}): ${b1H0TestStatistic}`)
+
+
+
